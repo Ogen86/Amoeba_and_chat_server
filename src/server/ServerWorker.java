cdd804bfc8b2c44fe499a37643698baafef7b183
Ogen86 <ogen86@gmail.com>
2019-09-02 13:44:38 +0200

    0.0.3

--- src/server/ServerWorker.java
+++ src/server/ServerWorker.java
@@ -1,5 +1,7 @@
 package server;
 
+// to do: list-be gyűjteni a playereket játék indításakor, játékszál a szerverbe beépítani metódusként
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -17,7 +19,8 @@ public class ServerWorker extends Thread {
 	private final Server server;
 	private String username = null;
 	private OutputStream outputStream;
-
+	private static File registry = new File("src/server/registry.txt");
+	
 	public ServerWorker(Server server, Socket clientSocket) {
 		this.server = server;
 		this.clientSocket = clientSocket;
@@ -51,13 +54,11 @@ public class ServerWorker extends Thread {
 				} else if ("J".equals(cmd)) {
 					handleLogin(outputStream, tokens);
 				} else if ("C".equals(cmd)) {
-					String[] tokensMsg = line.split(";", 3);
-					handleMessage(tokensMsg);
+					handleMessage(tokens);
 				} else if ("R".equals(cmd)) {
 					handleRegistry(tokens);
 				} else if ("P".equals(cmd)) {
-					String[] tokensMsg = line.split(";", 3);
-					handlePlayRequest(tokensMsg);
+					handlePlayRequest(tokens);
 				} else {
 					String msg = "unknown " + cmd + "\n";
 					outputStream.write(msg.getBytes());
@@ -70,10 +71,11 @@ public class ServerWorker extends Thread {
 
 	
 	private void handlePlayRequest(String[] tokens) throws IOException {
+		List<ServerWorker> workerList = server.getWorkerList();
 		if (tokens.length == 3) {
 			String username = tokens[1];
 			
-			List<ServerWorker> workerList = server.getWorkerList();
+			
 			for (ServerWorker worker : workerList) {
 				if (username.equalsIgnoreCase(worker.getLogin())) {
 					String outMsg = "P" + ";" + this.getLogin();
@@ -82,7 +84,13 @@ public class ServerWorker extends Thread {
 					BufferedReader rpReader = new BufferedReader(new InputStreamReader(response));
 					
 					if (rpReader.readLine() != null) {
-						if (rpReader.equals("A")) {
+						
+						if (rpReader.equals("A;" + worker.getLogin())) {
+							
+							String msg = "O;The game started";
+							outputStream.write(msg.getBytes());
+							OutputStream player2 = worker.outputStream;
+							player2.write(msg.getBytes());
 							
 							Socket clientSocket1 = this.clientSocket;
 							Socket clientSocket2 = worker.clientSocket;
@@ -92,11 +100,11 @@ public class ServerWorker extends Thread {
 							gameThread.run();
 							
 							
-						} else if (rpReader.equals("D")) {
-							String msg = username + " declined to play";
+						} else if (rpReader.equals("D;" + worker.getLogin())) {
+							String msg = "N;" + username + "; declined to play";
 							outputStream.write(msg.getBytes());
 						} else {
-							String msg = "error: unrecognised command \n";
+							String msg = "N;error: unrecognised command \n";
 							outputStream.write(msg.getBytes());
 							}
 					}
@@ -126,7 +134,7 @@ public class ServerWorker extends Thread {
 		List<ServerWorker> workerList = server.getWorkerList();
 
 		// send other online users current user's status
-		String onlineMsg = "offline " + username + "\n";
+		String onlineMsg = "L;" + username + "\n";
 		for (ServerWorker worker : workerList) {
 			if (!username.equals(worker.getLogin())) {
 				worker.send(onlineMsg);
@@ -140,13 +148,12 @@ public class ServerWorker extends Thread {
 	}
 	
 	private void handleRegistry(String[] tokens) throws IOException {
-		
 		if (tokens.length == 3) {
 			String username = tokens[1];
 			String password = tokens[2];
 		
-		Scanner sc = new Scanner(new File("src/server/registry.txt"));
-		PrintWriter pw = new PrintWriter(new File("src/server/registry.txt"));
+		Scanner sc = new Scanner(registry);
+		PrintWriter pw = new PrintWriter(registry);
 		
 			while (sc.hasNextLine()) {
 				String regLine = sc.nextLine();
@@ -157,20 +164,19 @@ public class ServerWorker extends Thread {
 		
 			if (!username.equals(regName)){
 				pw.println(username + ";" + password + ";" + "0");
-				String msg = "Successful registry\n";
+				String msg = "O;Successful registry\n";
 				outputStream.write(msg.getBytes());
 				this.username = username;
 				System.out.println("User registered in succesfully: " + username);
 
 				
 			} else {
-				String msg = "error: username occupied \n";
+				String msg = "N;error: username occupied \n";
 				outputStream.write(msg.getBytes());
 				System.err.println("registry failed for " + username);
 				}
 			}
-			sc.close();
-			pw.close();
+			
 		}
 	}
 
@@ -180,7 +186,7 @@ public class ServerWorker extends Thread {
 			String username = tokens[1];
 			String password = tokens[2];
 			
-			Scanner sc = new Scanner(new File("src/server/registry.txt"));
+			Scanner sc = new Scanner(registry);
 			while (sc.hasNextLine()) {
 				String regLine = sc.nextLine();
 				String[] regToken = regLine.split(";", 3);
@@ -199,14 +205,14 @@ public class ServerWorker extends Thread {
 				for (ServerWorker worker : workerList) {
 					if (worker.getLogin() != null) {
 						if (!username.equals(worker.getLogin())) {
-							String msg2 = worker.getLogin() + "online " + "\n";
+							String msg2 = "L;" + worker.getLogin();
 							send(msg2);
 						}
 					}
 				}
 
 				// send other online users current user's status
-				String onlineMsg = username + "online " + "\n";
+				String onlineMsg = "L;" + username;
 				for (ServerWorker worker : workerList) {
 					if (!username.equals(worker.getLogin())) {
 						worker.send(onlineMsg);
@@ -217,7 +223,7 @@ public class ServerWorker extends Thread {
 				outputStream.write(msg.getBytes());
 				System.err.println("Login failed for " + username);
 			}
-			sc.close();
+			
 		}
 		}
 	}
